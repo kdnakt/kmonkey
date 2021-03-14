67cfafcc6a4b11501ff2800166a6a11add07bc56
kdnakt <a.kid.1985@gmail.com>
2021-03-14 18:56:34 +0900

    add string()

--- src/nativeMain/kotlin/ast/ast.kt
+++ src/nativeMain/kotlin/ast/ast.kt
@@ -4,6 +4,7 @@ import token.Token
 
 interface Node {
     val tokenLiteral: String
+    fun string(): String
 }
 
 interface Statement: Node {
@@ -17,14 +18,53 @@ data class Program(val statements: List<Statement>): Node {
         statements.isNotEmpty() -> statements[0].tokenLiteral
         else -> ""
     }
+    override fun string(): String {
+        val sb = StringBuilder()
+        for (s in statements) {
+            sb.append(s.string())
+        }
+        return sb.toString()
+    }
 }
 
-data class LetStatement(val token: Token, val name: Identifier)
+data class LetStatement(val token: Token, val name: Identifier,
+        val value: Expression?): Statement {
+    override val tokenLiteral = token.literal
+    override fun string(): String {
+        val sb = StringBuilder("$tokenLiteral ")
+        sb.append(name.string())
+        sb.append(" = ")
+        if (value != null) sb.append(value.string())
+        sb.append(";")
+        return sb.toString()
+    }
+}
+
+data class ReturnStatement(val token: Token, val returnValue: Expression?)
     : Statement {
     override val tokenLiteral = token.literal
+    override fun string(): String {
+        val sb = StringBuilder("$tokenLiteral ")
+        if (returnValue != null) sb.append(returnValue.string())
+        sb.append(";")
+        return sb.toString()
+    }
+}
+
+data class ExpressionStatement(val token: Token, val expression: Expression?)
+    : Statement {
+    override val tokenLiteral = token.literal
+    override fun string(): String = expression!!.string()
 }
 
 data class Identifier(val token: Token, val value: String)
-    : Statement {
+    : Expression {
     override val tokenLiteral = token.literal
+    override fun string(): String = value
+}
+
+data class IntegerLiteral(val token: Token, val value: Long)
+    : Expression {
+    override val tokenLiteral = token.literal
+    override fun string(): String = value.toString()
 }
---
+++ src/nativeTest/kotlin/ast/AstTest.kt
@@ -0,0 +1,27 @@
+package ast
+
+import token.Token
+import token.TokenType
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class AstTest {
+    @Test
+    fun testString() {
+        val statements = listOf<Statement>(
+                LetStatement(
+                        Token(TokenType.LET, "let"),
+                        Identifier(
+                                Token(TokenType.IDENT, "myVar"),
+                                "myVar"
+                        ),
+                        Identifier(
+                                Token(TokenType.IDENT, "anotherVar"),
+                                "anotherVar"
+                        )
+                )
+        )
+        val program = Program(statements)
+        assertEquals("let myVar = anotherVar;", program.string())
+    }
+}
