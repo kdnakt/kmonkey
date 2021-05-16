e76da53bc94204772210044c98a81df830d0644b
kdnakt <a.kid.1985@gmail.com>
2021-05-16 11:15:01 +0900

    add unquote tests

--- src/nativeTest/kotlin/evaluator/QuoteUnquoteTest.kt
+++ src/nativeTest/kotlin/evaluator/QuoteUnquoteTest.kt
@@ -25,6 +25,21 @@ class QuoteUnquoteTest {
             "quote(unquote(4 + 4))" to "8",
             "quote(8 + unquote(4 + 4))" to "(8 + 8)",
             "quote(unquote(4 + 4) + 8)" to "(8 + 8)",
+            """
+                let foobar = 8;
+                quote(unquote(foobar))
+            """.trimIndent() to "8",
+            """
+                let foobar = 8;
+                quote(foobar)
+            """.trimIndent() to "foobar",
+            "quote(unquote(true))" to "true",
+            "quote(unquote(true == false))" to "false",
+            "quote(unquote(quote(4 + 4)))" to "(4 + 4)",
+            """
+                let quotedInfixExpression = quote(4 + 4);
+                quote(unquote(4 + 4) + unquote(quotedInfixExpression))
+            """.trimIndent() to "(8 + (4 + 4))"
         )
 
         for (test in tests) {
