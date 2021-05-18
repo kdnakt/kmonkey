b1f6dbd2bb44633e2d5071947728e460c8f06cdc
kdnakt <a.kid.1985@gmail.com>
2021-05-19 08:38:09 +0900

    add modify test

--- src/nativeTest/kotlin/ast/ModifyTest.kt
+++ src/nativeTest/kotlin/ast/ModifyTest.kt
@@ -4,24 +4,26 @@ import token.Token
 import token.TokenType
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
+
+val token = Token(TokenType.INT, "dummy")
+val ident = Identifier(token, "dummy")
+fun one(): Expression = IntegerLiteral(token, 1)
+fun two(): Expression = IntegerLiteral(token, 2)
+
+fun turnOneIntoTwo(node: Node?): Node {
+    if (node !is IntegerLiteral) {
+        return node!!
+    }
+    if (node.value != 1L) {
+        return node
+    }
+    return IntegerLiteral(node.token, 2)
+}
 
 class ModifyTest {
     @Test
     fun testModify() {
-        val token = Token(TokenType.INT, "dummy")
-        fun one(): Expression = IntegerLiteral(token, 1)
-        fun two(): Expression = IntegerLiteral(token, 2)
-
-        fun turnOneIntoTwo(node: Node?): Node {
-            if (node !is IntegerLiteral) {
-                return node!!
-            }
-            if (node.value != 1L) {
-                return node
-            }
-            return IntegerLiteral(node.token, 2)
-        }
-
         val tests = mapOf<Node, Node>(
             one() to two(),
             Program(mutableListOf(ExpressionStatement(token, one()))) to Program(mutableListOf(ExpressionStatement(token, two()))),
@@ -36,12 +38,31 @@ class ModifyTest {
                         BlockStatement(token, mutableListOf(ExpressionStatement(token, two()))),
                         BlockStatement(token, mutableListOf(ExpressionStatement(token, two())))),
             ReturnStatement(token, one()) to ReturnStatement(token, two()),
-            LetStatement(token, Identifier(token, "dummy"), one()) to LetStatement(token, Identifier(token, "dummy"), two()),
+            LetStatement(token, ident, one()) to LetStatement(token, ident, two()),
+            FunctionLiteral(token, listOf(ident), BlockStatement(token, mutableListOf(ExpressionStatement(token, one()))))
+                to FunctionLiteral(token, listOf(ident), BlockStatement(token, mutableListOf(ExpressionStatement(token, two())))),
+            ArrayLiteral(token, listOf(one(), one())) to ArrayLiteral(token, listOf(two(), two()))
         )
 
         for (test in tests) {
             val modified = modify(test.key, ::turnOneIntoTwo)
             assertEquals(test.value, modified)
+        }
+    }
+
+    @Test
+    fun testModifyHashLiteral() {
+        val hashLiteral = HashLiteral(token, mapOf(
+            one() to one(),
+            one() to one(),
+        ))
+        modify(hashLiteral, ::turnOneIntoTwo)
+
+        for (pair in hashLiteral.pairs) {
+            val key = pair.key as IntegerLiteral
+            assertEquals(2, key.value)
+            val value = pair.value as IntegerLiteral
+            assertEquals(2, value.value)
         }
     }
 }
